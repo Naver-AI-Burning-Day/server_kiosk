b6fdf4693bbde05de6027732713b5b44727b7b64
yeoneei <soso3786@gmail.com>
2020-02-13 21:57:30 +0900

    first api complete

--- routes/first.js
+++ routes/first.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var {user} = require('../models/user');
+var {user} = require('../models');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
     user.create({name:'temp'}).then(result=>{
-        console.log(result);
+        console.log(result.dataValues.id);
+        res.json({id :result.dataValues.id});
     })
-
 });
 module.exports = router;
