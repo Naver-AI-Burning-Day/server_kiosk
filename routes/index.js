69892780394f2d8124010b613d64fd7987e3cdd7
yeoneei <soso3786@gmail.com>
2020-02-13 20:49:34 +0900

    develop change

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const NaverChatbot = require('../module/OpenChat');
+
+router.post('/', async (req, res, next) => {
+    let config = {
+        CHATBOT_USER_ID: 'intotheunknown',
+        CHATBOT_TOKEN: 'Y0dJV3JabnBCblh4bm5wcnpQRkdndXFadHpvbWRBcUU=',
+        CHATBOT_URL: 'https://19vwdrsfxg.apigw.ntruss.com/send/beta/',
+    }
+    const chatbot = new NaverChatbot(config);
+    let data = await chatbot.answerText(req.body.text);
+    console.log(data);
+
+  });
 
 module.exports = router;
