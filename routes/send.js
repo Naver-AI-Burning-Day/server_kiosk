99aa7ca6ecc30916f309383cc2a0fa5cdcb89869
dnatuna <dnatuna123@gmail.com>
2020-02-13 20:13:28 +0900

    네이버 챗봇 연동 성공!

---
+++ routes/send.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const NaverChatbot = require('../NaverChatbot/index');
+
+router.post('/', async (req, res, next) => {
+    let config = {
+        CHATBOT_USER_ID: 'asdasdsadsd',
+        CHATBOT_TOKEN: 'Y0dJV3JabnBCblh4bm5wcnpQRkdndXFadHpvbWRBcUU=',
+        CHATBOT_URL: 'https://19vwdrsfxg.apigw.ntruss.com/send/beta/',
+    }
+    const chatbot = new NaverChatbot(config);
+    console.log(req.body.text);
+    let data = await chatbot.answerText(req.body.text);
+
+    console.log(data);
+});
+
+module.exports = router;
